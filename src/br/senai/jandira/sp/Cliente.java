9d53a6daa3e72380490b76dd86788ce995c55909
João Victor da Silva <jv147002@gmail.com>
2022-08-31 16:09:01 -0300

    Enviando de novo

--- src/br/senai/jandira/sp/Cliente.java
+++ src/br/senai/jandira/sp/Cliente.java
@@ -3,9 +3,11 @@ package br.senai.jandira.sp;
 public class Cliente {
 
 		public String nome;
-		public String email;
 		public String cidade;
 		public String idade;
+		public int teste;
+		public int teste2;
+		public int teste3;
 	
 		
 
